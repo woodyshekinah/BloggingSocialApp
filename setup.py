f0d162306bf350f834d68c6db07fb3e4a6ed3548
JustNimmu <168156633+MichelleWairimu@users.noreply.github.com>
2024-06-15 20:31:29 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Michelle Wairimu',
+    author_email= 'nimmunduati17@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
