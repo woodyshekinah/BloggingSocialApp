6ede8b8af4789c7dfbf478d456bf8c8188d38837
MichelleWairimu <nimmunduati17@gmail.com>
2024-07-12 16:01:10 +0300

    Commit

--- chatbot files/app.py
+++ chatbot files/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, jsonify, request
-from src.helper import download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
+from helper import download_hugging_face_embeddings
+from langchain_community.vectorstores import Pinecone
 import pinecone
 from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
+from langchain_community.llms import CTransformers
 from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
-from src.prompt import *
+from prompt import *
 import os
 
 app = Flask(__name__)
